c19f967de838e797aabb48afca4b4db543627134
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-04-03 15:06:52 +0200

    Added check for empty fields in AddBooks Form and added a dialog box on exit

--- SZB/AddBooks.cs
+++ SZB/AddBooks.cs
@@ -20,23 +20,38 @@ namespace SZB
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.Close();
-            Dashboard.restrict = 0;
+            if (MessageBox.Show("Czy jesteś pewny że chcesz wyjść?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes) {
+                this.Close();
+                Dashboard.restrict = 0;
+            }  
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string bname = textBox1.Text;
-            string bauthor = textBox2.Text;
-            string bpublication = textBox3.Text;
-            int price = Convert.ToInt32(textBox4.Text);
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+            {
+                MessageBox.Show("Puste pole niedozwolone", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string bname = textBox1.Text;
+                string bauthor = textBox2.Text;
+                string bpublication = textBox3.Text;
+                int price = Convert.ToInt32(textBox4.Text);
 
-            SqlConnection con = new SqlConnection(@"Data Source=librarymanagesystem.database.windows.net;Initial Catalog=SZB;User ID=adminXYZ;Password=GorzWlkp!;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            con.Open();
-            SqlCommand cmd = new SqlCommand("INSERT INTO Books (bName, bAuthor, bPublication, bQuantity) VALUES ('" + bname + "','" + bauthor+"', '" + bpublication + "','" + price + "')", con);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            MessageBox.Show("Dane zapisane", "sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                SqlConnection con = new SqlConnection(@"Data Source=librarymanagesystem.database.windows.net;Initial Catalog=SZB;User ID=adminXYZ;Password=GorzWlkp!;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Books (bName, bAuthor, bPublication, bQuantity) VALUES ('" + bname + "','" + bauthor + "', '" + bpublication + "','" + price + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Dane zapisane", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+            }  
         }
+
     }
 }
