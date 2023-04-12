7efa771ff31c8cadd83b21c45eec95fc43247dc9
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-04-12 21:41:58 +0200

    add functionality to button Delete in ViewBooks

--- SZB/ViewBooks.cs
+++ SZB/ViewBooks.cs
@@ -126,5 +126,24 @@ namespace SZB
                 MessageBox.Show("Please Refresh to see changes", "Success", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Dane will be deleted, Confirm?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                string bname = textBox2.Text;
+                string bauthor = textBox3.Text;
+                string publication = textBox4.Text;
+                int bquantity = Convert.ToInt32(textBox5.Text);
+
+                SqlConnection con = new SqlConnection(@"Data Source=librarymanagesystem.database.windows.net;Initial Catalog=SZB;User ID=adminXYZ;Password=GorzWlkp!;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                SqlCommand cmd = new SqlCommand("DELETE FROM Books WHERE bid=" + rowid + "", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                MessageBox.Show("Please Refresh to see changes", "Success", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
     }
 }
